45cee3a7abedbf1bf3881f40d1975709f517e9a9
MirageN1349 <desertirpipi@gmail.com>
2022-09-21 00:28:05 +0300

    feat: added docker

--- back/src/main.ts
+++ back/src/main.ts
@@ -32,7 +32,7 @@ async function bootstrap() {
   const app = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
     transport: Transport.TCP,
     options: {
-      host: 'localhost',
+      host: process.env.HOST,
       port: +process.env.PORT,
     },
   });
